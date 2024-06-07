7af7da54397928b397a3df2b75d6c19d30e1850c
Elbacalau <oriolreverte36@gmail.com>
2024-06-07 18:22:52 +0200

    Fix

--- prueba1.client/src/app/libros/libros.component.ts
+++ prueba1.client/src/app/libros/libros.component.ts
@@ -1,10 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { debounceTime } from 'rxjs';
-import { Subject } from 'rxjs';
 
-interface Libro {
-  id: number;
+export interface Libro {
+  id?: number;
   titulo: string;
   autor: string;
   dataPublicacion: number;
@@ -20,8 +18,8 @@ interface Libro {
 export class LibrosComponent implements OnInit{
   libros: Libro[] = [];
   terminoBusqueda = '';
-  libroEncontrado: Libro | null = null;
-  libroNoEncontrado: string = '';
+  libroEncontrado: Libro | undefined | null = null;
+  libroNoEncontrado: string | null  = null;
 
   constructor(private http: HttpClient) {}
 
@@ -43,22 +41,19 @@ export class LibrosComponent implements OnInit{
   buscarLibro() {
     // reiniciar el libro encontrado antes
     this.libroEncontrado = null;
+    this.libroNoEncontrado = null;
 
     if (!this.terminoBusqueda.trim()) {
       return;
     }
 
     const terminoBusquedaMin = this.terminoBusqueda.toLowerCase();
-    let encontrado: boolean = false;
 
-    this.libros.forEach(libro => {
-      if (libro.titulo.toLowerCase() === terminoBusquedaMin) {
-        this.libroEncontrado = libro;
-        return;
-      }
-    });
+    this.libroEncontrado = this.libros.find(libro =>
+      libro.titulo.toLowerCase() === terminoBusquedaMin
+    );
 
-    if (!encontrado) {
+    if (!this.libroEncontrado) {
       this.libroNoEncontrado = 'El libro no se ha encontrado.'
     }
   }
