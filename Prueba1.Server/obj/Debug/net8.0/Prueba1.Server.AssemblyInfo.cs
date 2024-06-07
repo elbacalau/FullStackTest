c01d0a85b511d3d3fc529ba7a94ad37f518ae79e
Elbacalau <oriolreverte36@gmail.com>
2024-06-07 16:54:43 +0200

    New Method FindBook

--- Prueba1.Server/obj/Debug/net8.0/Prueba1.Server.AssemblyInfo.cs
+++ Prueba1.Server/obj/Debug/net8.0/Prueba1.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba1.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d462e0c785df2c8767f466596c522435a7d28dd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c01dc24ae9840e760ac07e706bdbf9ea6021d61b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba1.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba1.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- prueba1.client/src/app/libros/libros.component.ts
+++ prueba1.client/src/app/libros/libros.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { debounceTime } from 'rxjs';
+import { Subject } from 'rxjs';
 
 interface Libro {
   id: number;
@@ -12,6 +15,51 @@ interface Libro {
   templateUrl: './libros.component.html',
   styleUrls: ['./libros.component.css']
 })
-export class LibrosComponent {
 
+
+export class LibrosComponent implements OnInit{
+  libros: Libro[] = [];
+  terminoBusqueda = '';
+  libroEncontrado: Libro | null = null;
+  libroNoEncontrado: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getLibro();
+  }
+
+  getLibro() {
+    this.http.get<Libro[]>('http://localhost:5130/Libro').subscribe(
+      (libros: Libro[]) => {
+        this.libros = libros;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  buscarLibro() {
+    // reiniciar el libro encontrado antes
+    this.libroEncontrado = null;
+
+    if (!this.terminoBusqueda.trim()) {
+      return;
+    }
+
+    const terminoBusquedaMin = this.terminoBusqueda.toLowerCase();
+    let encontrado: boolean = false;
+
+    this.libros.forEach(libro => {
+      if (libro.titulo.toLowerCase() === terminoBusquedaMin) {
+        this.libroEncontrado = libro;
+        return;
+      }
+    });
+
+    if (!encontrado) {
+      this.libroNoEncontrado = 'El libro no se ha encontrado.'
+    }
+  }
 }
